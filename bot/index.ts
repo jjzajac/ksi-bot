72afa247ad8173d2d5e563c8a66e1a5f21a5a3d2
jjzajac <41783233+jjzajac@users.noreply.github.com>
2022-03-24 19:47:27 +0100

    t

--- bot/index.ts
+++ bot/index.ts
@@ -1,16 +1,36 @@
 // Require the necessary discord.js classes
-import { Client, Intents } from 'discord.js'
+import { Client, Intents } from 'discord.js';
+import * as dotenv from 'dotenv';
 
-import * as dotenv from 'dotenv'
+dotenv.config();
 
-dotenv.config()
+const token = process.env.TOKEN;
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
 
-const token = process.env.TOKEN
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-
-client.once('ready', () => {
-  console.log('Ready!')
+client.on('guildMemberAdd',(m)=>{
+    
 })
 
-client.login(token)
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('messageCreate', async (m) => {
+  m.guild?.emojis.fetch()
+    .then((emojis) => emojis.forEach((v,k)=> m.react(v)))
+    .catch(console.error);
+    m.guild?.client.
+});
+
+
+(async () => {
+    await client.login(token);
+    client.emojis.cache.forEach((v,k)=>console.log(v,k));
+})();
